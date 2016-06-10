0e964d942c7f53ee6625bcb734bbe0eb2898a319
mculotta120 <margaret.culotta@gmail.com>
2016-06-10 15:26:56 -0500

    initial commit

---
+++ public/client.js
@@ -0,0 +1,10 @@
+ var valueX= document.getElementById('valueXIn').value;
+ var valueY= document.getElementById('valueYIn').value;
+//
+// $(function(){
+//
+//   $('#equals').click(function(){
+//   } // end equals
+//
+// });// end on load
+console.log(valueX + valueY);
